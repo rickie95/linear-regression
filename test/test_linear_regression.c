5ddcc2e633efd9b86b09b66a22003aa1d67d3dac
Riccardo Malavolti <riccardo.malavolti@stud.unifi.it>
2022-05-10 15:34:51 +0200

    removes conf boilerplate

--- test/test_linear_regression.c
+++ test/test_linear_regression.c
@@ -12,16 +12,12 @@ void test_assert_is_not_equal(void) {
     TEST_ASSERT_NOT_EQUAL(0, 1);
 }
 
-void test_assert_fails(void) {
-    TEST_ASSERT_NOT_EQUAL(1, 1);
-}
 
 int main(void) {
     UNITY_BEGIN();
 
     RUN_TEST(test_assert_is_equal);
     RUN_TEST(test_assert_is_not_equal);
-    RUN_TEST(test_assert_fails);
 
     return UNITY_END();
 }
